0ce8794e1b5c1b848d1b9c4daeb964dab95563e5
Reni Somoye <rensomoye@gmail.com>
2023-01-04 12:43:22 -0800

    commit tree 55a45150f027f73f0cdf5de8b4f6b341c909ae36

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+    return Math.abs(42 - block);
+}
+
+function distanceFromHqInFeet(block) {
+    return distanceFromHqInBlocks(block) * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs((start - destination) * 264)
+}    
+
+function calculatesFarePrice(start, destination) {
+    const totalDistance = distanceTravelledInFeet (start, destination)
+    if (totalDistance <= 400) {
+        return 0
+    } else if (totalDistance > 400 && totalDistance <= 2000){
+        return (totalDistance - 400) * .02
+    } else if (totalDistance > 2000 && totalDistance < 2500) {
+        return 25
+    } else {
+        return 'cannot travel that far'
+    }
+}
